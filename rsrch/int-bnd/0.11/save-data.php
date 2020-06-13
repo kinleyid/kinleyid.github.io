8f70ad48b39efe12048b4803dbc7a5e668a20896
kinleyid <isaac.kinley@gmail.com>
2020-06-13 17:00:29 -0400

    0.11 again

--- rsrch/int-bnd/0.11/save-data.php
+++
@@ -1,8 +0,0 @@
-<?php
-$filename = 'data/' . $_POST["pID"] . '.' . uniqid() . ".txt";
-$myfile = fopen($filename, "w") or die("Unable to open file!");
-fwrite($myfile, $_POST["txt"]);
-fclose($myfile);
-header('Location: end.html');
-die();
-?>
